e1f0d82c9d09bd01b40337f6ed1a83c55be3c87b
Jeslyn V <162588624+midnightwalks@users.noreply.github.com>
2025-06-10 16:48:58 +0700

    Update api.js

--- majusurat_fe/src/utils/api.js
+++ majusurat_fe/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken, saveToken, removeToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://majusurat-be-901699795850.us-central1.run.app/",
   withCredentials: true,
 });
 
